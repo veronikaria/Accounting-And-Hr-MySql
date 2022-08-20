9d7eca98bf668cc9c714c8f1ca6bac28c412e9e6
veronikaria <89734639+veronikaria@users.noreply.github.com>
2022-08-20 16:13:04 +0300

    Added description for queries

--- Queries2HRD.sql
+++ Queries2HRD.sql
@@ -1,12 +1,14 @@
 USE ResourcesDepartUniversity
 
+-- Display surnames, names of teachers, department code and name of the faculty
 SELECT l.LastName, l.FirstName, 
 d.Code AS department_code, 
 d.Name AS department_name 
 FROM Lecturer l, Department d
 WHERE l.DepartmentCode = d.Code
 
-
+-- Display class identifier, type, last name, teacher's name, department code and faculty name
+-- while sorting the sample by teacher's last name and first name
 SELECT lo.Id, lo.Type, l.LastName, l.FirstName, 
 d.Code AS department_code, 
 d.Name AS department_name 
@@ -15,8 +17,8 @@ WHERE l.DepartmentCode = d.Code
 AND lo.LecturerPersonnelNumber = l.PersonnelNumber
 ORDER BY l.LastName, l.FirstName
 
-
-
+-- Display the teacher's last name, date of birth, the department code and the name of the faculty
+-- while the department code must be either 'ІВПП' or 'ІПІ'
 SELECT l.LastName, l.Birthdate, 
 d.Code AS department_code, 
 d.Name AS department_name 
@@ -24,8 +26,8 @@ FROM Lecturer l JOIN  Department d
 ON l.DepartmentCode = d.Code
 WHERE d.Code IN ('ІВПП', 'ІПІ')
 
-
-
+-- Display class identifier, type, last name, teacher's name, department code and faculty name 
+-- while sorting the sample by teacher's last name and first name
 SELECT lo.Id, lo.Type, l.LastName, l.FirstName, 
 d.Code AS department_code, 
 d.Name AS department_name 
@@ -36,15 +38,20 @@ INNER JOIN Loading lo
 ON lo.LecturerPersonnelNumber = l.PersonnelNumber 
 ORDER BY l.LastName, l.FirstName
 
-
-
+-- Display the identifier, class type, last name and first name of the teacher. 
+-- And if there is no record for a certain teacher in the Loading table 
+-- then leave the fields from this table NULL. 
+-- And sort the sample by identifier
 SELECT lo.Id, lo.Type, l.LastName, l.FirstName
 FROM Lecturer l 
 LEFT OUTER JOIN Loading lo 
 ON lo.LecturerPersonnelNumber = l.PersonnelNumber 
 ORDER BY Id
 
-
+-- Display the identifier, class type, last name and first name of the teacher.
+-- And if there is no entry for a certain teacher in the Load table
+-- then do not display data about him. 
+-- And sort the sample by identifier
 SELECT lo.Id, lo.Type, l.LastName, l.FirstName
 FROM Lecturer l 
 RIGHT OUTER JOIN Loading lo 
